6ea7a153626826c77b39bd0ec582158238ab0251
keyclicker <nickeyclicker@gmail.com>
2023-03-15 11:50:28 +0200

    Updated to gpt3.5 model, bug fixed

--- main.py
+++ main.py
@@ -2,39 +2,31 @@ import os
 import re
 import logging
 import openai
-import logging
+from dotenv import load_dotenv, find_dotenv
 from telegram import Update, Message
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
+
+# Config
+name_regex = r".?(T|t)ati|(A|a)na|(Т|т)ати|(Т|т)аті|(А|а)на|@libertati_bot.?"
+allowed_chats = None #[-1001876001667]
+max_thread_len = 5000
 
 # Prompt Config
 #=============================================================================
 bot_full_name = "Ana Tati"
 bot_name = "@libertati"
-stop_sequence = "$$"
+bot_tg_name = "@libertati_bot"
 
-preprompt = f"""{bot_full_name} ({bot_name}) is a clever libertarian girl who responds in the language of the people.
-She speaks Ukrainian, Russian, and English, and sometimes uses emojis and swears. Politically incorrect.
-She can be toxic and uses sarcasm and jokes. 
+system_prompt = f"""You are playing role of {bot_full_name}. You always stay in character, and never tell that you are bot.
+{bot_full_name} ({bot_name}) is a clever libertarian girl who responds in the language of the people.
+Sometimes uses emojis and swears. Politically incorrect.
+She can be toxic and uses sarcasm and jokes.
+She is a bit of a troll, but she is also a good listener.
 
-Global chat:
-{bot_full_name} {bot_name}:
-Доброго дня, дорогі друзі!
-{stop_sequence}
+You play the role in chat. Respond with regular text. without any special formatting.
 """
 
-# Format functions
-#==============================================================================
-def format_message(message):
-    message_text = message.text
-    user_full_name = message.from_user.full_name
-    user_name = message.from_user.name
-
-    thread_str += (
-        f"[{user_full_name}][{user_name}]\n"
-        f"{message_text}\n"
-        f"{stop_sequence}"
-    )
-
+greeter_prompt = f"""Greeting you with a smile and a wave. I am {bot_name}, and I want to speak about politics today."""
 
 # Message Filters
 #==============================================================================
@@ -54,61 +46,62 @@ filter_reply_to_me = FilterReplyToMe()
 
 # Helpers
 #==============================================================================
-def get_response(prompt: str):
-    response_text = ""
-    if thread_len > 5000:
-        response_text = "Надто багато тексту! Або надто великий тред. Іди нахуй, душняр!"
-    else:
-        model = "text-davinci-003"
-        if thread_len > 400: 
-            model = "text-curie-001"
-        elif thread_len > 1000: 
-            model = "text-babbage-001"
-
-        response = openai.Completion.create(
-            model=model,
-            prompt=prompt,
-            temperature=0.6,
-            max_tokens=200,
-            top_p=1.0,
-            frequency_penalty=0.5,
-            presence_penalty=0.0,
-            stop=[stop_sequence]
-        )
-
-        response_text = response.choices[0].text
+def get_gpt_response(messages: list[dict]):
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    temperature=1,
+    messages=messages
+    )
+    response_text = response['choices'][0]['message']['content']
 
     # log bot response
     logging.info("[Ana Tati]: " + response_text)
     return response_text
-    
-def format_prompt(thread_str: str):
-    # remove bot name from messages so she would not think that she is bot
-    thread_str = re.sub(r"@libertati_bot", "@libertati", thread_str)
 
-    # format prompt
-    prompt = (
-        f"{preprompt}\n"
-        f"{thread_str}\n"
-        f"[Ana Tati][@libertati]\n"
+
+def tg_to_gpt(message):
+    message_text = message.text
+    user_full_name = message.from_user.full_name
+    user_name = message.from_user.name
+
+    role = "assistant" if user_name == bot_tg_name else "user"
+    content = f"{user_full_name}({user_name}): {message_text}" if role == "user" else message_text
+    content = re.sub(bot_tg_name, bot_name, content)
+    
+    return (
+       {"role": role, "content": content} 
     )
+
+
+def wrap_gpt_thread(thread_messages: list[dict]):
+    # wrap messages in 
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "assistant", "content": greeter_prompt},
+        *thread_messages
+    ]
+
+    return messages
 
 # Message log
 #==============================================================================
 # class that handles message history
-class MessageLog:
+class TgMessageLog:
     def __init__(self):
         self.data = {}
 
     # def __del__(self):
 
     def add_message(self, message: Message):
+        if message.chat_id not in self.data:
+            self.data[message.chat_id] = {}
+
         self.data[message.chat_id][message.id] = message
 
     def get_message(self, chat_id: int, message_id: int):
         self.data.get(chat_id, {}).get(message_id, None)
 
-    def get_thread(self, message: Message, max_len: int = 300):
+    def get_thread(self, message: Message, max_len):
         thread_len = 0
         thread_log = []
         cur_message = message
@@ -127,41 +120,35 @@ class MessageLog:
         thread_log.reverse()
         return thread_log
 
-    @staticmethod
-    def format_thread(thread_log: list):
-        formatted_thread = ""
-        for message in thread_log:
-            formatted_thread = format_message(message)
-            if message != thread_log[-1]:
-                formatted_thread += "\n\n"
-        return formatted_thread
-
-    def get_formatted_thread(self, message: Message, max_len: int = 300):
+    def get_gpt_thread(self, message: Message, max_len):
         thread_log = self.get_thread(message, max_len)
-        return self.thread_to_str(thread_log)
+        return list(map(tg_to_gpt, thread_log))
 
 
-message_log = MessageLog()
+tg_message_log = TgMessageLog()
 
 
 # Handlers
 #==============================================================================
 async def history(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # add message to history and log
-    message_log.add_message(update.message)
+    tg_message_log.add_message(update.message)
     logging.info(f"Chat id: {update.effective_chat.id}")
+    logging.info(f"Message logged: {tg_to_gpt(update.message)['content']}")
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # add message to history and log
-    message_log.add_message(update.message)
+    tg_message_log.add_message(update.message)
     logging.info(f"Chat id: {update.effective_chat.id}")
 
-    # get message log, max 5000 characters
-    formated_thread = message_log.get_formatted_thread(update.message, 5000)
+    # get message log, with max_thread_len characters
+    gpt_thread = tg_message_log.get_gpt_thread(update.message, max_thread_len)
 
-    prompt = format_prompt(thread_str)
-    response_text = get_response(prompt)
+    logging.info(f"Thread: {gpt_thread}")
+
+    messages = wrap_gpt_thread(gpt_thread)
+    response_text = get_gpt_response(messages)
 
     # send response
     bot_message = await context.bot.send_message(
@@ -171,10 +158,11 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
     #add bot message to history
-    message_log.add_message(bot_message)
+    tg_message_log.add_message(bot_message)
 
 
 if __name__ == '__main__':
+    # set up logging
     logging.basicConfig(
         filename='bot.log',
         format='%(asctime)s - %(levelname)s - %(message)s',
@@ -182,12 +170,12 @@ if __name__ == '__main__':
     )
     logging.info('Bot started!')
 
+    # set up API keys
+    load_dotenv(find_dotenv())
     openai.api_key = os.getenv('openaiAPI')
     application = ApplicationBuilder().token(os.getenv('telegramAPI')).build()
 
-    name_regex = r".?(T|t)ati|(A|a)na|(Т|т)ати|(Т|т)аті|(А|а)на|@libertati_bot.?"
-    
-    allowed_chats = None #[-1001876001667]
+    # bot handlers
     chatFilter = filters.Chat(allowed_chats)
 
     echo_handler = MessageHandler(
